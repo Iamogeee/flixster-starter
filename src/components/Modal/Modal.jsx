da11b52c98e03e58f93dd8764d779278860b807f
Ogenna Obiora <ogenna@meta.com>
2024-06-13 17:09:03 -0700

    added embedded youtube trailers

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./modal.css";
+import { useState, useEffect } from "react";
 
 const Genres = {
   28: "Action",
@@ -28,7 +29,33 @@ function mapGenres(genres) {
 }
 
 const Modal = ({ movie, onClose }) => {
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const getModalVideo = async (movieId) => {
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const videosUrl = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`;
+
+    const trailerUrl = await fetch(videosUrl)
+      .then((response) => response.json())
+      .then((response) =>
+        response.results.find(
+          (video) => video.site === "YouTube" && video.type === "Trailer"
+        )
+      )
+      .then((trailer) => `https://www.youtube.com/embed/${trailer.key}`)
+      .catch((error) => {
+        console.error("Error fetching movie trailer:", error);
+      });
+
+    //❗️CHANGE THE LINE BELOW TO SET YOUR TRAILER URL
+    setTrailerUrl(trailerUrl);
+  };
+
+  useEffect(() => {
+    getModalVideo(movie.id);
+  }, [movie]);
+
   if (!movie) return null;
+
   return (
     <div className="modal_popup" onClick={onClose}>
       <div className="modal-container" onClick={(e) => e.stopPropagation()}>
@@ -38,16 +65,31 @@ const Modal = ({ movie, onClose }) => {
         </div>
         <div className="modal-content">
           <div className="image-container">
-            <img
-              src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
-              alt={movie.title}
-            />
+            <div className="media-content1">
+              <img
+                className="modal-img"
+                src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                alt={movie.title}
+              />
+            </div>
+            <div className="media-content">
+              <iframe
+                src={trailerUrl} // CHANGE TRAILER VARIABLE TO YOURS
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Movie Trailer"
+                className="modal-movie-trailer" // CHANGE CLASS NAME TO YOURS
+              ></iframe>
+            </div>
           </div>
           <div className="bottom">
             <p> Release Date: {movie.release_date}</p>
             <p>Genres: {mapGenres(movie.genre_ids)}</p>
             <p>Overview: {movie.overview}</p>
           </div>
+
+          <div></div>
+
           <button onClick={onClose}>Close</button>
         </div>
       </div>
